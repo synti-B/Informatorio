e05b6075c34d8e3c0decdb2e3b40fcd2f778e601
CristianIvan00 <cristiancarballo929@gmail.com>
2021-12-07 10:17:06 -0300

    proyecto

---
+++ ProyectoInformatorio/app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from.views import home, logueo, post , nuevologin
+urlpatterns = [
+    path('', home, name="home"),
+    path('logueo/', logueo, name="logueo"),
+    path('post/', post, name="post"),
+    path('nuevologin/', nuevologin, name="nuevologin"),
+
+]
---
+++ ProyectoInformatorio/app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'app/home.html' )
+
+def logueo(request):
+    return render(request, 'app/logueo.html')
+
+def post(request):
+    return render(request, 'app/post.html' )
+    
+def nuevologin(request):
+    return render(request, 'app/nuevologin.html')

628d3edeb9cb9f99aebe249825a1968a59b74c36
synti <synti.bogado@gmail.com>
2021-12-08 00:34:12 -0300

    actualizacion

--- ProyectoInformatorio/ProyectoInformatorio/settings/base.py
+++ ProyectoInformatorio/ProyectoInformatorio/settings/base.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ProyectoInformatorio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(BASE_DIR),'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 

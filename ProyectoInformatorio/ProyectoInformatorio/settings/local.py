af2a55a2c027d15450ae7ddce15290fa0e82ac00
CristianIvan00 <cristiancarballo929@gmail.com>
2021-12-09 09:30:29 -0300

    se conecto la base de datos

--- ProyectoInformatorio/ProyectoInformatorio/settings/local.py
+++ ProyectoInformatorio/ProyectoInformatorio/settings/local.py
@@ -1,1 +1,13 @@
 from .base import *
+
+
+DATABASES = {
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'configuracion',
+        'USER': 'rootest',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306',
+    }
+}
--- ProyectoInformatorio/app/models.py
+++ ProyectoInformatorio/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Post(models.Model):
+    author= models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title= models.CharField(max_length=260)
+    content= models.TextField()
+    img= models.ImageField(null=True, blank=True, upload_to='img', help_text='Seleccione una imagen para mostrar')
+    created_on= models.DateTimeField(default=timezone.now)
+    last_modified= models.DateTimeField(auto_now=True)
+    published_date= models.DateTimeField(null=True, blank=True)
+    categorias= models.ManyToManyField('category', related_name='posts')
